3941448075bb5e49e2a9d520ff5b3e662bb5c3b5
Amba <80679383+amba05@users.noreply.github.com>
2022-06-03 01:37:53 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-import sys
 
-num = len(sys.argv)
+if __name__ == "__main__":
+    """Print argument length and arguments"""
+    import sys
 
-if num == 1:
-    print("{} arguments.".format(num - 1))
-else:
-    print("{} arguments:\n".format(num - 1))
+    num = len(sys.argv)
 
-    i = 1
-    for elem in sys.argv:
-        if elem != sys.argv[0]:
-            print("{}: {}\n".format(i, elem))
-            i+=1
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    else:
+        print("{} arguments:\n".format(num - 1))
+
+        i = 1
+        for elem in sys.argv:
+            if elem != sys.argv[0]:
+                print("{}: {}\n".format(i, elem))
+                i+=1
