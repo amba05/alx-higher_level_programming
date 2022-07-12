ba93b2263105e79cc3dff4c5051edee295875585
Amba <80679383+amba05@users.noreply.github.com>
2022-07-12 22:38:00 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,7 +29,7 @@ class Base:
             Returns: the JSON string representation of list_dictionaries
         '''
 
-        if list_dictionaries is None or json_string == "[]":
+        if list_dictionaries is None or list_dictionaries == "[]":
             return "[]"
         return json.dumps(list_dictionaries)
 
