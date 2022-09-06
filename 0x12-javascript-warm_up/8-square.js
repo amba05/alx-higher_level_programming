bd138ae4b358f12b6d5c3b6bdb9dcb6f8aae04bd
amba05 <emma.amba@gmail.com>
2022-09-06 03:16:34 +0000

    Javascript update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const size = parseInt(process.argv[2]);
 if (isNaN(size)) console.log('Missing size');
 
 for (let i = 1; i <= size; i++) {
-	const temp = [];
-	for (let j = 1; j <= size; j++) temp.push('X');
-	console.log(temp.join(''));
+  const temp = [];
+  for (let j = 1; j <= size; j++) temp.push('X');
+  console.log(temp.join(''));
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,7 +4,7 @@ const first = parseInt(process.argv[2]);
 const second = parseInt(process.argv[3]);
 
 function add (a, b) {
-	  console.log(a + b);
+  console.log(a + b);
 }
 
 add(first, second);
