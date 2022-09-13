1d967a66df72cac7377d115988b9177e42a834f8
Amba <80679383+amba05@users.noreply.github.com>
2022-09-14 00:39:08 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa
+Your script should take 3 arguments:
+mysql username, mysql password and database name
+"""
 
 import sys
 import MySQLdb
